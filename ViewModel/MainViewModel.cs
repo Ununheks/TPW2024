9d67009a1225a399f139af7ffe34ff96347e0454
Ununheks <ununheks@gmail.com>
2024-04-21 16:36:09 +0200

    Added flag to button press

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -13,6 +13,7 @@ namespace ViewModel
         private ModelAPI _modelLayer;
         private int _ballCount;
         private string _ballCountText;
+        private bool _isStarted = false;
 
         public ObservableCollection<IBall> Balls { get; } = new ObservableCollection<IBall>();
 
@@ -20,7 +21,7 @@ namespace ViewModel
         {
             _modelLayer = ModelAPI.CreateService();
             IDisposable observer = _modelLayer.Subscribe<IBall>(x => Balls.Add(x));
-            StartCommand = new RelayCommand(Start);
+            StartCommand = new RelayCommand(Start, () => !_isStarted);
         }
 
         public string BallCountText
@@ -42,10 +43,12 @@ namespace ViewModel
             if (int.TryParse(BallCountText, out _ballCount) && _ballCount > 0)
             {
                 _modelLayer.Start(_ballCount);
+                _isStarted = true; 
+                RaisePropertyChanged(() => StartCommand); 
             }
             else
             {
-
+                // Handle invalid input
             }
         }
 
